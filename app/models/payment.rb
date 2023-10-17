8e8284c9ced265d3bcb37669741916c67431a1d7
clin2on3mun <munanaclinton@gmail.com>
2023-10-17 11:45:21 +0200

    Update models

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,7 +1,7 @@
 class Payment < ApplicationRecord
   belongs_to :user
+  has_many :category_payments
   has_many :categories, through: :category_payments
-  has_many :payments
 
   validates :name, presence: true
   validates :amount, numericality: { greater_than_or_equal_to: 0.0 }
