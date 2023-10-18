628d2ad57a66e61e0e930c897b64cd9006aae22b
clin2on3mun <munanaclinton@gmail.com>
2023-10-18 14:39:09 +0200

    Add spec for models

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user_1) { User.create(name: 'Munana', email: 'munana@gmail.com', password: '123456') }
+  let (:category) {Category.create(name: 'hospital', icon: 'hospital', user: user_1) }
+  describe 'validations' do
+    it "category to be valid" do
+      expect(category).to be_valid
+    end
+    it "category to be valid" do
+      category.name = nil
+      expect(category).to_not be_valid
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user_1) { User.create(name: 'Munana', email: 'munana@gmail.com', password: '123456') }
+  describe 'validations' do
+    it "the user to be valid" do
+      expect(user_1).to be_valid
+    end
+    it "the user name should be provided" do
+      user_1.name = nil
+      expect(user_1).to_not be_valid
+    end
+    it "the email should valid should have /@/." do
+      user_1.email = 'hack'
+      expect(user_1).to_not be_valid
+    end
+  end
 end
